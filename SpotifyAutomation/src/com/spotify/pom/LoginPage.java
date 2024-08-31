05d2e17f9949cd0d49ea6513f7c102f23780bdd5
Asha <Asha@FFE-AshaRani-FFE127>
2024-08-31 12:05:01 +0530

    First Commit

---
+++ SpotifyAutomation/src/com/spotify/pom/LoginPage.java
@@ -0,0 +1,30 @@
+package com.spotify.pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+//initialization
+public class LoginPage {
+@FindBy(id="login-username")
+private WebElement unTbx;
+@FindBy(id="login-password")
+private WebElement pwdTbx;
+@FindBy(xpath = "//span[text()='Log In']")
+private WebElement loginBtn;
+@FindBy(xpath = "//span[text()='Log in']")
+private WebElement loginLink;
+
+//Declaration
+public LoginPage(WebDriver driver) {
+	PageFactory.initElements(driver, this);
+}
+
+//Utilization
+public void login(String un,String pwd) {
+	loginLink.click();
+	unTbx.sendKeys(un);
+	pwdTbx.sendKeys(pwd);
+	loginBtn.click();
+}
+}
